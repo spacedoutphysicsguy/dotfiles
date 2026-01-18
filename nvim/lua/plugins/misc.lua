91d1b76397096456292ce398d1a79b3316a3c629
Prithvi Singh <prithvisindhu9@gmail.com>
2026-01-18 03:20:00 +0100

    linux compliant config

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,59 +1,52 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
-  {
-    -- autoclose tags
-    'windwp/nvim-ts-autotag',
-  },
-  {
-    -- detect tabstop and shiftwidth automatically
-    'tpope/vim-sleuth',
-  },
-  {
-    -- Powerful Git integration for Vim
-    'tpope/vim-fugitive',
-  },
-  {
-    -- GitHub integration for vim-fugitive
-    'tpope/vim-rhubarb',
-  },
-  {
-    -- Hints keybinds
-    'folke/which-key.nvim',
-    opts = {
-      delay = 3000,
-      -- win = {
-      --   border = {
-      --     { '┌', 'FloatBorder' },
-      --     { '─', 'FloatBorder' },
-      --     { '┐', 'FloatBorder' },
-      --     { '│', 'FloatBorder' },
-      --     { '┘', 'FloatBorder' },
-      --     { '─', 'FloatBorder' },
-      --     { '└', 'FloatBorder' },
-      --     { '│', 'FloatBorder' },
-      --   },
-      -- },
-    },
-  },
-  {
-    -- Autoclose parentheses, brackets, quotes, etc.
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    opts = {},
-  },
-  {
-    -- Highlight todo, notes, etc in comments
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
-  },
-  {
-    -- high-performance color highlighter
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end,
-  },
+	{
+		-- autoclose tags
+		"windwp/nvim-ts-autotag",
+	},
+	{
+		-- detect tabstop and shiftwidth automatically
+		"tpope/vim-sleuth",
+	},
+	{
+		-- Powerful Git integration for Vim
+		"tpope/vim-fugitive",
+	},
+	{
+		-- GitHub integration for vim-fugitive
+		"tpope/vim-rhubarb",
+	},
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		-- Highlight todo, notes, etc in comments
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{
+		-- high-performance color highlighter
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+
+	-- Selector for working with different python venvs
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+		config = true,
+		event = "VeryLazy",
+		keys = {
+			-- Keymap to open the selector (e.g., Leader + v)
+			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+		},
+	},
 }
---
+++ nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,21 @@
+return {
+	-- Hints keybinds
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		delay = 0,
+		preset = "helix",
+		-- win = {
+		--   border = {
+		--     { '┌', 'FloatBorder' },
+		--     { '─', 'FloatBorder' },
+		--     { '┐', 'FloatBorder' },
+		--     { '│', 'FloatBorder' },
+		--     { '┘', 'FloatBorder' },
+		--     { '─', 'FloatBorder' },
+		--     { '└', 'FloatBorder' },
+		--     { '│', 'FloatBorder' },
+		--   },
+		-- },
+	},
+}
