d08ee0c2a3122bde066a443850a7531e8187d9e4
Prithvi Singh <prithvisindhu9@gmail.com>
2026-04-28 09:42:45 +0100

    lsp changes

--- nvim/lua/core/snippets.lua
+++ nvim/lua/core/snippets.lua
@@ -5,49 +5,46 @@
 vim.hl.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
 
 -- Appearance of diagnostics
-vim.diagnostic.config {
-  virtual_text = {
-    prefix = '●',
-    -- Add a custom format function to show error codes
-    format = function(diagnostic)
-      local code = diagnostic.code and string.format('[%s]', diagnostic.code) or ''
-      return string.format('%s %s', code, diagnostic.message)
-    end,
-  },
-  underline = false,
-  update_in_insert = true,
-  float = {
-    source = true, -- Or "if_many"
-  },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = ' ',
-      [vim.diagnostic.severity.WARN] = ' ',
-      [vim.diagnostic.severity.INFO] = ' ',
-      [vim.diagnostic.severity.HINT] = '󰌵 ',
-    },
-  },
-  -- Make diagnostic background transparent
-  on_ready = function()
-    vim.cmd 'highlight DiagnosticVirtualText guibg=NONE'
-  end,
-}
-
--- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.hl.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●",
+		-- Add a custom format function to show error codes
+		format = function(diagnostic)
+			local code = diagnostic.code and string.format("[%s]", diagnostic.code) or ""
+			return string.format("%s %s", code, diagnostic.message)
+		end,
+	},
+	underline = false,
+	update_in_insert = true,
+	float = {
+		source = true, -- Or "if_many"
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = "󰌵 ",
+		},
+	},
+	-- Make diagnostic background transparent
+	on_ready = function()
+		vim.cmd("highlight DiagnosticVirtualText guibg=NONE")
+	end,
 })
 
--- Set kitty terminal padding to 0 when in nvim
--- vim.cmd [[
---   augroup kitty_mp
---   autocmd!
---   au VimLeave * :silent !kitty @ set-spacing padding=default margin=default
---   au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0 3 0 3
---   augroup END
--- ]]
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.hl.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
+vim.api.nvim_create_autocmd("CmdlineChanged", {
+	callback = function()
+		vim.api.nvim_call_function("wildtrigger", {})
+	end,
+})
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -43,97 +43,96 @@ vim.lsp.config("lua_ls", {
 })
 
 vim.lsp.enable({ "lua_ls", "jsonls", "sqlls", "yamlls", "ty", "ruff", "bashls", "ltex", "texlab" })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-	-- Create a function that lets us more easily define mappings specific LSP related items.
-	-- It sets the mode, buffer and description for us each time.
-	callback = function(event)
-		local map = function(keys, func, desc)
-			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
-		end
-
-		local builtin = require("telescope.builtin")
-		-- Jump to the definition of the word under your cursor.
-		--  This is where a variable was first declared, or where a function is defined, etc.
-		--  To jump back, press <C-T>.
-		map("<leader>ld", builtin.lsp_definitions, "Goto [D]efinition")
-
-		-- Find references for the word under your cursor.
-		map("<leader>lr", builtin.lsp_references, "Goto [R]eferences")
-
-		-- Jump to the implementation of the word under your cursor.
-		--  Useful when your language has ways of declaring types without an actual implementation.
-		map("<leader>li", builtin.lsp_implementations, "Goto [I]mplementation")
-
-		-- Jump to the type of the word under your cursor.
-		--  Useful when you're not sure what type a variable is and you want to see
-		--  the definition of its *type*, not where it was *defined*.
-		map("<leader>lD", builtin.lsp_type_definitions, "Type [D]efinition")
-
-		-- Fuzzy find all the symbols in your current document.
-		--  Symbols are things like variables, functions, types, etc.
-		map("<leader>fds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
-
-		-- Fuzzy find all the symbols in your current workspace
-		--  Similar to document symbols, except searches over your whole project.
-		map("<leader>fws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-		-- Rename the variable under your cursor
-		--  Most Language Servers support renaming across files, etc.
-		map("<leader>lc", vim.lsp.buf.rename, "[C]hange Name")
-
-		-- Execute a code action, usually your cursor needs to be on top of an error
-		-- or a suggestion from your LSP for this to activate.
-		map("<leader>la", vim.lsp.buf.code_action, "Code [A]ction")
-		-- Opens a popup that displays documentation about the word under your cursor
-		--  See `:help K` for why this keymap
-		map("K", vim.lsp.buf.hover, "Hover Documentation")
-		map("L", vim.lsp.buf.signature_help, "Signature Help")
-		-- WARN: This is not Goto Definition, this is Goto Declaration.
-		-- For example, in C this would take you to the header
-		-- map("<leader>lD", vim.lsp.buf.declaration, "Goto [D]eclaration")
-
-		map("<leader>lwa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-		map("<leader>lwr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-		map("<leader>lwl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, "[W]orkspace [L]ist Folders")
-
-		-- The following two autocommands are used to highlight references of the
-		-- word under your cursor when your cursor rests there for a little while.
-		--    See `:help CursorHold` for information about when this is executed
-		-- When you move your cursor, the highlights will be cleared (the second autocommand).
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-			local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
-				callback = vim.lsp.buf.document_highlight,
-			})
-
-			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
-				callback = vim.lsp.buf.clear_references,
-			})
-
-			vim.api.nvim_create_autocmd("lspDetach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-				callback = function(event2)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-				end,
-			})
-		end
-
-		-- The following code creates a keymap to toggle inlay hints in your
-		-- code, if the language server you are using supports them
-		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-			map("<leader>lh", function()
-				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-			end, "Toggle Inlay [H]ints")
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+-- 	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+-- 	-- Create a function that lets us more easily define mappings specific LSP related items.
+-- 	-- It sets the mode, buffer and description for us each time.
+-- 	callback = function(event)
+-- 		local map = function(keys, func, desc)
+-- 			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
+-- 		end
+--
+-- 		local builtin = require("telescope.builtin")
+-- 		-- Jump to the definition of the word under your cursor.
+-- 		--  This is where a variable was first declared, or where a function is defined, etc.
+-- 		--  To jump back, press <C-T>.
+-- 		map("<leader>ld", builtin.lsp_definitions, "Goto [D]efinition")
+--
+-- 		-- Find references for the word under your cursor.
+-- 		map("<leader>lr", builtin.lsp_references, "Goto [R]eferences")
+--
+-- 		-- Jump to the implementation of the word under your cursor.
+-- 		--  Useful when your language has ways of declaring types without an actual implementation.
+-- 		map("<leader>li", builtin.lsp_implementations, "Goto [I]mplementation")
+--
+-- 		-- Jump to the type of the word under your cursor.
+-- 		--  Useful when you're not sure what type a variable is and you want to see
+-- 		--  the definition of its *type*, not where it was *defined*.
+-- 		map("<leader>lD", builtin.lsp_type_definitions, "Type [D]efinition")
+--
+-- 		-- Fuzzy find all the symbols in your current document.
+-- 		--  Symbols are things like variables, functions, types, etc.
+-- 		map("<leader>fds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+--
+-- 		-- Fuzzy find all the symbols in your current workspace
+-- 		--  Similar to document symbols, except searches over your whole project.
+-- 		map("<leader>fws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+--
+-- 		-- Rename the variable under your cursor
+-- 		--  Most Language Servers support renaming across files, etc.
+-- 		map("<leader>lc", vim.lsp.buf.rename, "[C]hange Name")
+--
+-- 		-- Execute a code action, usually your cursor needs to be on top of an error
+-- 		-- or a suggestion from your LSP for this to activate.
+-- 		map("<leader>la", vim.lsp.buf.code_action, "Code [A]ction")
+-- 		-- Opens a popup that displays documentation about the word under your cursor
+-- 		--  See `:help K` for why this keymap
+-- 		map("K", vim.lsp.buf.hover, "Hover Documentation")
+-- 		map("L", vim.lsp.buf.signature_help, "Signature Help")
+-- 		-- WARN: This is not Goto Definition, this is Goto Declaration.
+-- 		-- For example, in C this would take you to the header
+-- 		-- map("<leader>lD", vim.lsp.buf.declaration, "Goto [D]eclaration")
+--
+-- 		map("<leader>lwa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+-- 		map("<leader>lwr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+-- 		map("<leader>lwl", function()
+-- 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+-- 		end, "[W]orkspace [L]ist Folders")
+--
+-- 		-- The following two autocommands are used to highlight references of the
+-- 		-- word under your cursor when your cursor rests there for a little while.
+-- 		--    See `:help CursorHold` for information about when this is executed
+-- 		-- When you move your cursor, the highlights will be cleared (the second autocommand).
+-- 		local client = vim.lsp.get_client_by_id(event.data.client_id)
+-- 		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+-- 			local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+-- 			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+-- 				buffer = event.buf,
+-- 				group = highlight_augroup,
+-- 				callback = vim.lsp.buf.document_highlight,
+-- 			})
+--
+-- 			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+-- 				buffer = event.buf,
+-- 				group = highlight_augroup,
+-- 				callback = vim.lsp.buf.clear_references,
+-- 			})
+--
+-- 			vim.api.nvim_create_autocmd("lspDetach", {
+-- 				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+-- 				callback = function(event2)
+-- 					vim.lsp.buf.clear_references()
+-- 					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+-- 				end,
+-- 			})
+-- 		end
+--
+-- 		-- The following code creates a keymap to toggle inlay hints in your
+-- 		-- code, if the language server you are using supports them
+-- 		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+-- 			map("<leader>lh", function()
+-- 				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+-- 			end, "Toggle Inlay [H]ints")
+-- 		end
+-- 	end,
+-- })
